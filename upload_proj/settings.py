4553f7864a83f9d2432cbcfbe5d685d925ff4f47
Thiago Fernandes <thiago.sousa@ifg.edu.br>
2022-06-26 18:46:00 -0300

    add
    add

--- upload_proj/settings.py
+++ upload_proj/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+import django_on_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
