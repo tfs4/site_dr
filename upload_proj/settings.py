cb803cd7a9192f96bb6cb4e472c5902b91a987d2
Thiago Fernandes <44616457+tfs4@users.noreply.github.com>
2022-12-27 13:56:09 -0300

    Update settings.py

--- upload_proj/settings.py
+++ upload_proj/settings.py
@@ -121,7 +121,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-CSRF_TRUSTED_ORIGINS = ['https://dr-level.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://dr-level.up.railway.app']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
